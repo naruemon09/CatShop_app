8a6665fda80e2d361aeea3e31d552288dbe69481
Jutharat Pooamorn <day28645@gmail.com>
2025-07-02 14:17:39 +0700

    post resgister

--- CatShop_app/src/components/Register.jsx
+++ CatShop_app/src/components/Register.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [form , setForm] = useState({
+    username : "",
+    firstname : "",
+    lastname : "",
+    email : "",
+    birthdate : "",
+    phone : "",
+    address : "",
+    password : "",
+  });
+
+  const onSubmit = async () => {
+    try {
+      const response = await axios.post("http://localhost:7092/api/register" , form);
+      alert("Register Successed")
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <section style={{ background: 'url("./src/images/background-img.png")' }}>
       <div class="container">
@@ -11,42 +35,60 @@ const Register = () => {
             <form>
               <div class="mb-3">
                 <input type="text" class="form-control form-control-lg" name="username"
+                  value={form.username}
+                  onChange={(e) => setForm({ ...form, username: e.target.value })}
                   placeholder="Enter Your Username" />
               </div>
               <div class="mb-3">
                 <input type="text" class="form-control form-control-lg" name="firstname"
+                  value={form.firstname}
+                  onChange={(e) => setForm({ ...form, firstname: e.target.value })}
                   placeholder="Enter Your Firstname" />
               </div>
               <div class="mb-3">
                 <input type="text" class="form-control form-control-lg" name="lastname"
+                  value={form.lastname}
+                  onChange={(e) => setForm({ ...form, lastname: e.target.value })}
                   placeholder="Enter Your Lastname" />
               </div>
               <div class="mb-3">
                 <input type="email" class="form-control form-control-lg" name="email"
+                  value={form.email}
+                  onChange={(e) => setForm({ ...form, email: e.target.value })}
                   placeholder="Enter Your Email Address" />
               </div>
               <div class="mb-3">
-                <input type="date" class="form-control form-control-lg" name="birthdate" />
+                <input type="date" class="form-control form-control-lg" name="birthdate" 
+                  value={form.birthdate}
+                  onChange={(e) => setForm({ ...form, birthdate: e.target.value })}/>
               </div>
               <div class="mb-3">
                 <input type="text" class="form-control form-control-lg" name="gender"
+                  value={form.gender}
+                  onChange={(e) => setForm({ ...form, gender: e.target.value })}
                   placeholder="Enter Your Gender" />
               </div>
               <div class="mb-3">
                 <input type="tel" class="form-control form-control-lg" name="phone"
+                  value={form.phone}
+                  onChange={(e) => setForm({ ...form, phone: e.target.value })}
                   placeholder="Enter Your Phone" />
               </div>
               <div class="mb-3">
                 <input type="text" class="form-control form-control-lg" name="address"
+                  value={form.address}
+                  onChange={(e) => setForm({ ...form, address: e.target.value })}
                   placeholder="Enter Your Address" />
               </div>
               <div class="mb-3">
                 <input type="password" class="form-control form-control-lg" name="password"
+                  value={form.password}
+                  onChange={(e) => setForm({ ...form, password: e.target.value })}
                   placeholder="Enter Your Password" />
               </div>
 
               <div class="d-grid gap-2">
-                <button type="submit" class="btn btn-dark btn-lg rounded-1">Register it now</button>
+                <button type="submit" class="btn btn-dark btn-lg rounded-1" onClick={onSubmit}>Register it now</button>
               </div>
 
               <div class="mt-4 text-center">
