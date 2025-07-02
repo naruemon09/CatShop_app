4bfb03a8347a45ae743f23d82b897e91fcca2021
NaruemonKhongao <156495906+NaruemonKhongao@users.noreply.github.com>
2025-07-02 16:45:14 +0700

    commit

--- CatShop_app/src/components/Register.jsx
+++ CatShop_app/src/components/Register.jsx
@@ -1,56 +1,76 @@
 import React from 'react'
 
 const Register = () => {
+
   return (
     <section style={{ background: 'url("./src/images/background-img.png")' }}>
-      <div class="container">
-        <div class="row">
-          <div class="offset-md-3 col-md-6 my-5">
-            <h2 class="display-3 fw-normal text-center">...<span class="text-primary">Register</span>
+      <div className="container">
+        <div className="row">
+          <div className="offset-md-3 col-md-6 my-5">
+            <h2 className="display-3 fw-normal text-center">...<span className="text-primary">Register</span>
             </h2>
             <form>
-              <div class="mb-3">
-                <input type="text" class="form-control form-control-lg" name="username"
+              <div className="mb-3">
+                <input type="text" className="form-control form-control-lg" name="username"
                   placeholder="Enter Your Username" />
               </div>
-              <div class="mb-3">
-                <input type="text" class="form-control form-control-lg" name="firstname"
-                  placeholder="Enter Your Firstname" />
-              </div>
-              <div class="mb-3">
-                <input type="text" class="form-control form-control-lg" name="lastname"
-                  placeholder="Enter Your Lastname" />
-              </div>
-              <div class="mb-3">
-                <input type="email" class="form-control form-control-lg" name="email"
-                  placeholder="Enter Your Email Address" />
-              </div>
-              <div class="mb-3">
-                <input type="date" class="form-control form-control-lg" name="birthdate" />
-              </div>
-              <div class="mb-3">
-                <input type="text" class="form-control form-control-lg" name="gender"
-                  placeholder="Enter Your Gender" />
-              </div>
-              <div class="mb-3">
-                <input type="tel" class="form-control form-control-lg" name="phone"
-                  placeholder="Enter Your Phone" />
-              </div>
-              <div class="mb-3">
-                <input type="text" class="form-control form-control-lg" name="address"
-                  placeholder="Enter Your Address" />
-              </div>
-              <div class="mb-3">
-                <input type="password" class="form-control form-control-lg" name="password"
+              <div className="mb-3">
+                <input type="password" className="form-control form-control-lg" name="password"
                   placeholder="Enter Your Password" />
               </div>
+              <div className="row">
+                <div className="col-md-6 mb-3">
+                  <input type="text" className="form-control form-control-lg" name="firstname"
+                    placeholder="Enter Your Firstname" />
+                </div>
+                <div className="col-md-6 mb-3">
+                  <input type="text" className="form-control form-control-lg" name="lastname"
+                    placeholder="Enter Your Lastname" />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-6 mb-3">
+                  <input type="date" className="form-control form-control-lg" name="birthdate" />
+                </div>
 
-              <div class="d-grid gap-2">
-                <button type="submit" class="btn btn-dark btn-lg rounded-1">Register it now</button>
+                <div className="col-md-6 mt-2">
+                  <label className="form-control-lg">Gender :</label>
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="male"
+                    // checked={gender === 0}
+
+                    />
+                    <label className="px-2">Male</label>
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="female"
+                    // checked={gender === 1}
+                    />
+                    <label className="px-2">Female</label>
+                </div>
+              </div>
+              <div className="mb-3">
+                <input type="email" className="form-control form-control-lg" name="email"
+                  placeholder="Enter Your Email Address" />
+              </div>
+              <div className="mb-3">
+                <input type="tel" className="form-control form-control-lg" name="phone"
+                  placeholder="Enter Your Phone" />
+              </div>
+              <div className="mb-3">
+                <textarea type="text" className="form-control form-control-lg" name="address"
+                  placeholder="Enter Your Address" />
+              </div>
+              
+              <div className="d-grid gap-2">
+                <button type="submit" className="btn btn-dark btn-lg rounded-1">Register it now</button>
               </div>
 
-              <div class="mt-4 text-center">
-                <p class="mb-0 fw-normal">Have already an account? <a href="/login" class="fw-bold text-primary">Sign in</a></p>
+              <div className="mt-4 text-center">
+                <p className="mb-0 fw-normal">Have already an account? <a href="/login" className="fw-bold text-primary">Sign in</a></p>
               </div>
             </form>
           </div>
