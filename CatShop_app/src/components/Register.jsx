a03bbaa01dfd07a31dcc4f13f46e4f4d995be176
NaruemonKhongao <156495906+NaruemonKhongao@users.noreply.github.com>
2025-07-02 21:25:34 +0700

    commit

--- CatShop_app/src/components/Register.jsx
+++ CatShop_app/src/components/Register.jsx
@@ -1,23 +1,30 @@
+import axios from "axios";
 import React, { useState } from "react";
-import axios from 'axios'
 import { useNavigate } from "react-router-dom";
 
+
 const Register = () => {
-  const navigate = useNavigate();
-  const [form , setForm] = useState({
-    username : "",
-    firstname : "",
-    lastname : "",
-    email : "",
-    birthdate : "",
-    phone : "",
-    address : "",
-    password : "",
+
+  const navigate = useNavigate()
+
+  const [form, setForm] = useState({
+    username: "",
+    firstname: "",
+    lastname: "",
+    email: "",
+    birthdate: "",
+    phone: "",
+    address: "",
+    password: "",
+    gender: ""
   });
 
   const onSubmit = async () => {
+
     try {
+      console.log(form)
       const response = await axios.post("http://localhost:7092/api/register" , form);
+      console.log(response)
       alert("Register Successed")
       navigate("/login");
     } catch (error) {
@@ -30,74 +37,124 @@ const Register = () => {
       <div className="container">
         <div className="row">
           <div className="offset-md-3 col-md-6 my-5">
-            <h2 className="display-3 fw-normal text-center">...<span className="text-primary">Register</span>
+            <h2 className="display-3 fw-normal text-center">
+              ...
+              <span className="text-primary">Register</span>
+              ...
             </h2>
-            <form>
-              <div className="mb-3">
-                <input type="text" className="form-control form-control-lg" name="username"
-                  placeholder="Enter Your Username" />
-              </div>
-              <div className="mb-3">
-                <input type="password" className="form-control form-control-lg" name="password"
-                  placeholder="Enter Your Password" />
-              </div>
-              <div className="row">
-                <div className="col-md-6 mb-3">
-                  <input type="text" className="form-control form-control-lg" name="firstname"
-                    placeholder="Enter Your Firstname" />
-                </div>
-                <div className="col-md-6 mb-3">
-                  <input type="text" className="form-control form-control-lg" name="lastname"
-                    placeholder="Enter Your Lastname" />
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-6 mb-3">
-                  <input type="date" className="form-control form-control-lg" name="birthdate" />
-                </div>
 
-                <div className="col-md-6 mt-2">
-                  <label className="form-control-lg">Gender :</label>
-                    <input
-                      type="radio"
-                      name="gender"
-                      value="male"
-                    // checked={gender === 0}
+            <div className="mb-3">
+              <input type='text'
+                className="form-control form-control-lg"
+                name="username"
+                value={form.username}
+                onChange={(e) => setForm({ ...form, username: e.target.value })}
+                placeholder="Enter Your Username" />
+            </div>
 
-                    />
-                    <label className="px-2">Male</label>
-                    <input
-                      type="radio"
-                      name="gender"
-                      value="female"
-                    // checked={gender === 1}
-                    />
-                    <label className="px-2">Female</label>
-                </div>
+            <div className="mb-3">
+              <input type='password'
+                className="form-control form-control-lg"
+                name="password" value={form.password}
+                onChange={(e) => setForm({ ...form, password: e.target.value })}
+                placeholder="Enter Your Password" />
+            </div>
+
+            <div className="row">
+              
+              <div className="col-md-6 mb-3">
+                <input type='text'
+                  className="form-control form-control-lg"
+                  name="firstname" value={form.firstname}
+                  onChange={(e) => setForm({ ...form, firstname: e.target.value })}
+                  placeholder="Enter Your Firstname" />
               </div>
-              <div className="mb-3">
-                <input type="email" className="form-control form-control-lg" name="email"
-                  placeholder="Enter Your Email Address" />
+
+              <div className="col-md-6 mb-3">
+                <input type='text'
+                  className="form-control form-control-lg"
+                  name="lastname" value={form.lastname}
+                  onChange={(e) => setForm({ ...form, lastname: e.target.value })}
+                  placeholder="Enter Your Lastname" />
               </div>
-              <div className="mb-3">
-                <input type="tel" className="form-control form-control-lg" name="phone"
-                  placeholder="Enter Your Phone" />
-              </div>
-              <div className="mb-3">
-                <textarea type="text" className="form-control form-control-lg" name="address"
-                  placeholder="Enter Your Address" />
+
+            </div>
+
+            <div className="row">
+              
+              <div className="col-md-6 mb-3">
+                <input type="date"
+                  className="form-control form-control-lg"
+                  name="birthdate"
+                  value={form.birthdate}
+                  onChange={(e) => setForm({ ...form, birthdate: e.target.value })}
+                  placeholder="birthdate" />
               </div>
               
-              <div className="d-grid gap-2">
-                <button type="submit" className="btn btn-dark btn-lg rounded-1">Register it now</button>
+              <div className="col-md-6 mt-2">
+                <label className="form-control-lg">Gender :</label>
+                <input
+                  type="radio"
+                  name="gender"
+                  value={"0"}
+                  checked={form.gender === '0'}
+                  onChange={(e) => setForm({ ...form, gender: e.target.value })}/>
+                <label className="px-2">Male</label>
+                
+                <input
+                  type="radio"
+                  name="gender"
+                  value={"1"}
+                  checked={form.gender === '1'}
+                  onChange={(e) => setForm({ ...form, gender: e.target.value })}/>
+                <label className="px-2">Female</label>
               </div>
 
-              <div className="mt-4 text-center">
-                <p className="mb-0 fw-normal">Have already an account? <a href="/login" className="fw-bold text-primary">Sign in</a></p>
-              </div>
-            </form>
+            </div>
+
+            <div className="mb-3">
+              <input type="email"
+                className="form-control form-control-lg"
+                name="email"
+                value={form.email}
+                onChange={(e) => setForm({ ...form, email: e.target.value })}
+                placeholder="Enter Your Email Address" />
+            </div>
+
+            <div className="mb-3">
+              <input type="tel"
+                className="form-control form-control-lg"
+                name="phone"
+                value={form.phone}
+                onChange={(e) => setForm({ ...form, phone: e.target.value })}
+                placeholder="Enter Your Phone" />
+            </div>
+
+            <div className="mb-3">
+              <textarea type="text" c
+                lassName="form-control form-control-lg"
+                name="address"
+                value={form.address}
+                onChange={(e) => setForm({ ...form, address: e.target.value })}
+                placeholder="Enter Your Address" />
+            </div>
+
+            <div className="d-grid gap-2">
+              <button
+                type="submit"
+                className="btn btn-dark btn-lg rounded-1"
+                onClick={() => { onSubmit() }}>Register it now
+              </button>
+            </div>
+
+            <div className="mt-4 text-center">
+              <p className="mb-0 fw-normal">Have already an account?
+                <a href="/login" className="fw-bold text-primary">Sign in</a>
+              </p>
+            </div>
+
           </div>
-        </div>
+        </div>       
       </div>
     </section>
   )
